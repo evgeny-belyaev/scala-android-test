5071cff17d6889505a13d4a2ac7ed10465f3128a
evgeniy.belyaev <evgeny.belyaev@gmail.com>
2014-07-01 13:26:53 +0400

    Initial commit

---
+++ src/com/ugene/scalaandroid/Main.java
@@ -0,0 +1,15 @@
+package com.ugene.scalaandroid;
+
+//public class Main extends Activity
+//{
+//    /** Called when the activity is first created. */
+//    @Override
+//    public void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//
+//        int i = new Test().p();
+//
+//        setContentView(R.layout.main);
+//    }
+//}
---
+++ src/com/ugene/scalaandroid/Test.scala
@@ -0,0 +1,13 @@
+package com.ugene.scalaandroid
+
+import android.app.Activity
+import android.os.Bundle
+
+class Main extends Activity {
+  override def onCreate(savedInstanceState: Bundle) = {
+    super.onCreate(savedInstanceState)
+
+    setContentView(R.layout.main)
+  }
+}
+
